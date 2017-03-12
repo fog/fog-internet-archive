0c08551c34ed8b876eb8c8e8a3832279acd24305
Nat Welch <nat@natwelch.com>
2017-03-12 10:30:01 -0700

    move stuff around

--- lib/fog/internet_archive.rb
+++ lib/fog/internet_archive.rb
@@ -1,1 +1,9 @@
-require 'fog/internet_archive/storage'
+require "fog/core"
+require "fog/xml"
+require "fog/internet_archive/version"
+
+module Fog
+  module Storage
+    autoload :InternetArchive, File.expand_path("../storage/internet_archive", __FILE__)
+  end
+end
